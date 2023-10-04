ea26b94ca9cf4c1304b8749ed2f79cb3945806ae
yoyo <a4253607@gmail.com>
2023-10-04 23:37:03 +0800

    Update 2023-10-04T15:36:59.738Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -51,7 +51,8 @@ var jsData = [{
   'description': {
     des1: 'DOM',
     des2: 'AJAX'
-  }
+  },
+  'aos': "fade-down-right"
 }, {
   'id': 1,
   'imgUrl': './assets/images/woworoom.jpg',
@@ -62,25 +63,60 @@ var jsData = [{
     des2: 'POST',
     des3: 'PUT',
     des4: 'DELETE'
-  }
+  },
+  'aos': "fade-down"
 }, {
-  'id': 1,
+  'id': 2,
   'imgUrl': './assets/images/woworoom-backend.jpg',
   'title': 'WOWOROOM-BackEnd',
   'link': 'https://joe-1021.github.io/wowoRoom/backEnd',
   'description': {
     des1: '訂單紀錄',
     des2: 'C3.js'
+  },
+  'aos': "fade-down-left"
+}];
+var ReactData = [{
+  'id': 0,
+  'imgUrl': './assets/images/reactTodo.jpg',
+  'title': 'Todo',
+  'link': 'https://joe-1021.github.io/vite-HW4/',
+  'description': {
+    des1: '註冊、登入',
+    des2: '新增、刪除 Todo'
+  }
+}, {
+  'id': 1,
+  'imgUrl': './assets/images/searchSomething.jpg',
+  'title': 'SearchSomething',
+  'link': 'https://joe-1021.github.io/SearchSomething/',
+  'description': {
+    des1: '搜尋',
+    des2: '串接搜尋api'
+  }
+}, {
+  'id': 2,
+  'imgUrl': './assets/images/order.jpg',
+  'title': 'Order',
+  'link': 'https://joe-1021.github.io/Order/',
+  'description': {
+    des1: '訂單系統'
   }
 }];
 var jsPortList = document.querySelector('.js-portfolio');
+var reactPortList = document.querySelector('.react-portfolio');
 
 function renderData() {
   var content = '';
   jsData.forEach(function (item) {
-    content += "<li class=\"col\">\n              <div class=\"card h-100\">\n                <div class=\"Portfolio-pic border-0\">\n                  <img src=\"".concat(item.imgUrl, "\" alt=\"Art-NFT\" class=\"img-fluid\">\n                  <div class=\"img-cover d-none d-lg-flex\">\n                    <a href=\"").concat(item.link, "\" class=\"fs-2 text-white\" target=\"_blank\">more</a>\n                  </div>\n                </div>\n                <div class=\"\">\n                  <h3 class=\"p-3 text-center border-bottom  fs-3 fw-bold\">").concat(item.title, "</h3>\n                  <ul class=\"p-2 fs-5\">\n                    <li class=\"red-dot\"><p class=\"ms-3\">").concat(item.description.des1, "</p></li>\n                    <li class=\"red-dot\"><p class=\"ms-3\">").concat(item.description.des2, "</p></li>\n                    <li class=\"red-dot\"><p class=\"ms-3\">").concat(item.description.des3, "</p></li>\n                    <li class=\"red-dot\"><p class=\"ms-3\">").concat(item.description.des4, "</p></li>\n                  </ul>\n                  <div class=\"d-flex mt-auto d-lg-none\">\n                    <a href=\"").concat(item.link, "\" class=\"btn btn-primary w-100\">\u9032\u5165\u7DB2\u7AD9</a>\n                  </div>\n                </div>\n              </div>\n            </li>");
+    content += "<li class=\"col\">\n              <div class=\"card h-100\">\n                <div class=\"Portfolio-pic border-0\">\n                  <img src=\"".concat(item.imgUrl, "\" alt=\"").concat(item.title, "\" class=\"img-fluid\">\n                  <div class=\"img-cover d-none d-lg-flex\">\n                    <a href=\"").concat(item.link, "\" class=\"fs-2 text-white\" target=\"_blank\">more</a>\n                  </div>\n                </div>\n                <div class=\"\">\n                  <h3 class=\"p-3 text-center border-bottom  fs-3 fw-bold\">").concat(item.title, "</h3>\n                  <ul class=\"p-2 fs-5\">\n                    <li class=\"red-dot\"><p class=\"ms-3\">").concat(item.description.des1, "</p></li>\n                    <li class=\"red-dot\"><p class=\"ms-3\">").concat(item.description.des2, "</p></li>\n                    <li class=\"red-dot\"><p class=\"ms-3\">").concat(item.description.des3, "</p></li>\n                    <li class=\"red-dot\"><p class=\"ms-3\">").concat(item.description.des4, "</p></li>\n                  </ul>\n                  <div class=\"d-flex mt-auto d-lg-none\">\n                    <a href=\"").concat(item.link, "\" class=\"btn btn-primary w-100\">\u9032\u5165\u7DB2\u7AD9</a>\n                  </div>\n                </div>\n              </div>\n            </li>");
   });
-  jsPortList.innerHTML = content; //把作品描述為 undefined 隱藏
+  jsPortList.innerHTML = content;
+  var reactContent = '';
+  ReactData.forEach(function (item) {
+    reactContent += "\n    <li class=\"col\">\n      <div class=\"card h-100\">\n        <div class=\"Portfolio-pic border-0\">\n          <img src=\"".concat(item.imgUrl, "\" alt=\"").concat(item.title, "\" class=\"img-fluid\">\n          <div class=\"img-cover d-none d-lg-flex\">\n            <a href=\"").concat(item.link, "\" class=\"fs-2 text-white\" target=\"_blank\">more</a>\n          </div>\n        </div>\n        <div class=\"\">\n          <h3 class=\"p-3 text-center border-bottom  fs-3 fw-bold\">").concat(item.title, "</h3>\n          <ul class=\"p-2 fs-5\">\n            <li class=\"red-dot\"><p class=\"ms-3\">").concat(item.description.des1, "</p></li>\n            <li class=\"red-dot\"><p class=\"ms-3\">").concat(item.description.des2, "</p></li>\n            <li class=\"red-dot\"><p class=\"ms-3\">").concat(item.description.des3, "</p></li>\n            <li class=\"red-dot\"><p class=\"ms-3\">").concat(item.description.des4, "</p></li>\n          </ul>\n          <div class=\"d-flex mt-auto d-lg-none\">\n            <a href=\"").concat(item.link, "\" class=\"btn btn-primary w-100\">\u9032\u5165\u7DB2\u7AD9</a>\n          </div>\n        </div>\n      </div>\n    </li>\n    ");
+  });
+  reactPortList.innerHTML = reactContent; //把作品描述為 undefined 隱藏
 
   var desList = document.querySelectorAll('.red-dot');
   desList.forEach(function (i) {
